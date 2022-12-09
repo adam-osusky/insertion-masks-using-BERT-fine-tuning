ae4b7303b1a967973d07c7b08ad75a054e919b17
Adam Osusky <adam.osusky02@gmail.com>
2022-12-09 18:48:22 +0100

    Fix loading

--- insertion-masks.py
+++ insertion-masks.py
@@ -13,8 +13,6 @@ import transformers
 import evaluate
 import numpy as np
 
-seqeval = evaluate.load("seqeval")
-tokenizer = AutoTokenizer.from_pretrained(arg.model)
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--target-prob", default="")
@@ -23,27 +21,7 @@ parser.add_argument("--seed", default=69, type=int, help="Random seed")
 parser.add_argument("--dataset_name", default="inserted_words_dataset.jsonl", help="name")
 parser.add_argument("--model", default="distilbert-base-uncased")
 
-
-def tokenize_and_align_labels(examples):
-    tokenized_inputs = tokenizer(examples["words"], truncation=True, is_split_into_words=True)
-
-    labels = []
-    for i, label in enumerate(examples["target"]):
-        word_ids = tokenized_inputs.word_ids(batch_index=i)  # Map tokens to their respective word.
-        previous_word_idx = None
-        label_ids = []
-        for word_idx in word_ids:  # Set the special tokens to -100.
-            if word_idx is None:
-                label_ids.append(-100)
-            elif word_idx != previous_word_idx:  # Only label the first token of a given word.
-                label_ids.append(label[word_idx])
-            else:
-                label_ids.append(-100)
-            previous_word_idx = word_idx
-        labels.append(label_ids)
-
-    tokenized_inputs["labels"] = labels
-    return tokenized_inputs
+seqeval = evaluate.load("seqeval")
 
 
 def compute_metrics(p):
@@ -81,7 +59,31 @@ label2id = {
 def main(args: argparse.Namespace):
     access_token = "kkt"
 
-    dataset = load_dataset("wikitext", "wikitext-103-v1", split="train")
+    tokenizer = AutoTokenizer.from_pretrained(args.model)
+
+    def tokenize_and_align_labels(examples):
+        tokenized_inputs = tokenizer(examples["words"], truncation=True, is_split_into_words=True)
+
+        labels = []
+        for i, label in enumerate(examples["target"]):
+            word_ids = tokenized_inputs.word_ids(batch_index=i)  # Map tokens to their respective word.
+            previous_word_idx = None
+            label_ids = []
+            for word_idx in word_ids:  # Set the special tokens to -100.
+                if word_idx is None:
+                    label_ids.append(-100)
+                elif word_idx != previous_word_idx:  # Only label the first token of a given word.
+                    label_ids.append(label[word_idx])
+                else:
+                    label_ids.append(-100)
+                previous_word_idx = word_idx
+            labels.append(label_ids)
+
+        tokenized_inputs["labels"] = labels
+        return tokenized_inputs
+
+    dataset = load_dataset("json", data_files=args.dataset_name, split="train")
+    print(dataset)
     # tokenizer = AutoTokenizer.from_pretrained(arg.model)
     tokenized_data = dataset.map(tokenize_and_align_labels, batched=True)
     data_collator = DataCollatorForTokenClassification(tokenizer=tokenizer)
@@ -115,7 +117,7 @@ def main(args: argparse.Namespace):
 
     trainer.train()
     # trainer.push_to_hub()
-
+
     return 0
 
 
