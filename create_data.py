a08cd9b2231b2592eb1116eb2407ef131679ce79
Adam Osusky <adam.osusky02@gmail.com>
2022-12-09 09:14:49 +0100

    Create json from generated data.

--- create_data.py
+++ create_data.py
@@ -2,9 +2,11 @@
 import argparse
 import random
 import sys
+from tqdm import tqdm
 
 # imports
 from datasets import load_dataset
+import pandas as pd
 
 parser = argparse.ArgumentParser()
 
@@ -27,14 +29,16 @@ def get_word_vocab(dataset):
 
 def main(args: argparse.Namespace):
     dataset = load_dataset("wikitext", "wikitext-103-v1", split="test")
-    print()
     print(dataset)
 
     vocab = get_word_vocab(dataset)
     vocab_size = len(vocab)  # [0, vocab_size)
+    print("Vocabulary size: ", vocab_size)
 
+    print("Generating data")
     generated_data = []
-    for instance in dataset:
+    count = 0
+    for instance in tqdm(dataset):
         words = instance["text"].split()
         num_words = len(words)
         num_to_insert = int(num_words * args.insert_proportion)
@@ -51,10 +55,39 @@ def main(args: argparse.Namespace):
         for i, w in enumerate(words):
             new_words.append(w)
             if i in insertions:
-                new_words.append(inserions[i])
+                new_words.append(insertions[i])
                 targets[i + inserted + 1] = 1
                 inserted += 1
-        break
+        # print("total words: ", num_words, "To insert: ", num_to_insert)
+        # print("insert_idxs :", insert_idxs)
+        # print(insertions)
+        # print("targets len:", len(targets))
+        # print(targets)
+        # # for i, w in zip(new_words, targets):
+        # #     print(i, w)
+        # print("--------")
+
+        if len(new_words) == 0:
+            continue
+
+        example = {
+            "words": new_words,
+            "target": targets
+        }
+        
+        generated_data.append(example)
+
+        # count += 1
+        # if count > 3:
+        #     break
+
+    print("Creating json")
+    df = pd.DataFrame.from_records(generated_data)
+    df.to_json("inserted_words_dataset.jsonl", orient="records", lines=True)
+
+    print("creating hugface object")
+    generated_dataset = load_dataset("json", data_files="inserted_words_dataset.jsonl", split="train")
+    print(generated_dataset)
 
     return 0
 
@@ -62,4 +95,3 @@ def main(args: argparse.Namespace):
 if __name__ == "__main__":
     args = parser.parse_args([] if "__file__" not in globals() else None)
     x = main(args)
-    print(x)
